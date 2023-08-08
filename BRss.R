3b7f5467f12ed3fb1f7efef0aac91788a6e9df6a
bianm <68303004+bianm@users.noreply.github.com>
2023-08-08 14:35:12 -0400

    Add files via upload

---
+++ BRss.R
@@ -0,0 +1,43 @@
+##bx is the naive estimate and se_x is the corresponding standard error, 
+##num_boot is the number of bootstrap you can specify (1000 or greater is recommended)
+##nobs is the number of observation for the G-X association
+BRss <- function (bx,se_x,num_boot,nobs){
+  z=bx/se_x
+  M <- length(bx)
+  boot_diff <- rep(0,M)
+  for(i in 1:num_boot){
+    #dd is the matrix with 2 columns. 
+    #The first column is for saving the within-sample bootstrap estimate
+    #The second column is for saving the corrected out-of-sample bootstrap estimate
+    dd = matrix(NA,M,2)
+    #p_de is vector about the correlation between within-sample bootstrap estimate
+    #and out-of-sample bootstrap estimate
+    p_de <- -(1+2*bx^2)*sqrt(exp(1)^(-1))/(1-bx^2)
+    dd[,1]=rnorm(M,mean=bx,sd=sqrt((1-bx^2)/nobs))
+    dd[,2]=rnorm(M,mean=bx,sd=sqrt((1-bx^2)/(exp(1)^(-1)*nobs)*(1-p_de^2)))
+    chis <- rchisq(M,df=nobs-1)
+    #Simulate the variance of within-sample bootstrap estimate from chi-squared distribution
+    vd_star <- (1-bx^2)/nobs/(nobs-1) * chis
+    #Obtain the z-statistics based on the within-sample estimates
+    z_star <- dd[,1]/sqrt(vd_star)
+    #Get the order of z-stats based on the within-sample estimates
+    ind <- order(z_star)
+    #Reorder the bootstrap estimates and also the original estimate based on z-stats from the with-sample estimate
+    boot.beta.e=dd[,2][ind]
+    beta.d=dd[,1][ind]
+    beta.N=bx[ind]
+    #Correct the out-of-sample bootstrap estimate by accounting for the negative correlation
+    boot_diff <- boot_diff + (beta.d-boot.beta.e)
+  }
+  #Get the rank of naive (original) z stats
+  rank_z <- rank(z)
+  #Averaging over the bootstrap difference
+  boot_diff <- boot_diff/ num_boot
+  #correct the naive estimate by matching the rank of re-ordered z-statistics.
+  c.bx = bx - boot_diff[rank_z]
+  #Ensure that the corrected estimate is less than the original estimate in magnitude
+  ind1 <- which(abs(c.bx) > abs(bx))
+  if(length(ind1) >0){c.bx[ind1] <- bx[ind1]}
+  #Return the corrected estimates for G-X association
+  c.bx
+}
